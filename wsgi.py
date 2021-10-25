381a3534dae33d738b39ad39469ffca1b62b4fbe
HSN <hasanlock@gmail.com>
2021-10-25 22:46:28 +0600

    feat: setup CORS
    - init /pockets api

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,23 @@
+# flask application and middleware
+# -------------
 from flask import Flask
-from health_check.api import check
+from flask_cors import CORS
 
-# flas application
 app = Flask(__name__)
+CORS(app)
+
 
 # setup routes
+# -------------
+from health_check.api import check
+from pocket.api import store
+
 app.add_url_rule(
     '/health',
     view_func=check,
     methods=['GET'])
+
+app.add_url_rule(
+    '/pockets',
+    view_func=store,
+    methods=['POST'])
