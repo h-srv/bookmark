0de9b969945047e4611bdf46c99cbe23d58187eb
HSN <hasanlock@gmail.com>
2021-10-25 22:55:24 +0600

    feat: setup SQLAlchemy

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,11 @@
 # -------------
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+db = SQLAlchemy(app)
 
 
 # setup routes
