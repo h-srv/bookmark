1e9795134361440116b99425c75d4798f5384d76
HSN <hasanlock@gmail.com>
2021-10-26 08:31:57 +0600

    feat: setup sqlite db
    - temporary, to be replace with postgres

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 # flask application and middleware
 # -------------
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# CORS
 CORS(app)
+
+# DB
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
