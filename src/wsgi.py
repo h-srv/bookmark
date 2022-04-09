87e4a2b0dbc2e53f2efaa7b45a7bb4e610009a6f
HSN <hasanlock@gmail.com>
2022-04-09 13:00:27 +0600

    feat: setup CORS and migrate

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,42 +1,40 @@
-# ----------------------------------
-# flask application and middleware
-# ----------------------------------
+"""
+Bookmark flask application and middleware
+"""
+
 import os
+
+from .bookmark.health_check import api as health_check_api
+from .bookmark.infrastructure import db, cors, migrate
+from .bookmark.tag.api import store as tag_store
 from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
 
 
 def create_app():
+    """
+    Create app
+    :return:
+    """
     app = Flask(__name__)
 
     # load db config and init db
-    from .bookmark.infrastructure import db
-
     app.config['SQLALCHEMY_DATABASE_URI'] = '{DB_DRIVER}://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'.format(**os.environ)
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-    db.init_app(app)
+    db.init_app(app)  # setup db
+    cors.init_app(app)  # setup cors
+    migrate.init_app(app, db)  # setup migrate
+
+    routes(app)  # load routes
 
     return app
 
-
-# # CORS
-# CORS(app)
-
-# Migration
-# migrate = Migrate(app, db)
-
-# from .bookmark.tag.domain import Pocket
-
-# ----------------------------------
-# setup routes
-# ----------------------------------
-from .bookmark.health_check import api as health_check_api
-from .bookmark.tag.api import store as tag_store
-
-app = create_app()
-
-app.add_url_rule('/health', view_func=health_check_api, methods=['GET'])
-app.add_url_rule('/pockets', view_func=tag_store, methods=['POST'])
+def routes(app: Flask):
+    """
+    setup routes
+    :param app:
+    :return:
+    """
+    app.add_url_rule('/health', view_func=health_check_api, methods=['GET'])
+    app.add_url_rule('/pockets', view_func=tag_store, methods=['POST'])
