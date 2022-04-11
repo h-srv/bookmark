11219e877ee027e3a53890472746a436b210daa7
HSN <hasanlock@gmail.com>
2022-04-11 21:31:19 +0600

    refactor: update tag model

--- src/bookmark/tag/domain.py
+++ src/bookmark/tag/domain.py
@@ -1,11 +1,15 @@
 from ..infrastructure import db
+from ..shared.domain import Model
 
 
 # Pocket model
-class Tag(db.Model):
+class Tag(Model):
     __tablename__ = 'tags'
+    __table_args__ = (db.UniqueConstraint('id', 'user_rel', name='uniq_tags_id_user_rel'), )
 
-    id = db.Column(db.Integer, primary_key=True)
+    user_rel = db.Column(db.String(64), nullable=True)  # may be bigint or some other hash
     title = db.Column(db.String(1024), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=True)
-    updated_at = db.Column(db.DateTime, nullable=True)
+
+
+class TagRepository:
+    pass
