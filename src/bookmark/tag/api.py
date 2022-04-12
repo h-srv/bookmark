6926b82105e8a7621cac582b73f112e04159d43e
HSN <hasanlock@gmail.com>
2022-04-12 20:46:08 +0600

    feat: create tag api

--- src/bookmark/tag/api.py
+++ src/bookmark/tag/api.py
@@ -1,20 +1,26 @@
 from flask_restx import Namespace, Resource, fields
 
-tag_ns = Namespace('api/v1/tags', description='Tag api')
+from .domain import TagRepository, TagDTO
 
+
+tag_ns = Namespace('api/v1/tags', description='Tag api')
 tag = tag_ns.model('Tag', {
     'id': fields.Integer(readonly=True, description='The tag unique identifier'),
     'title': fields.String(required=True, description='The tag title'),
     'user_rel': fields.String(required=False, description='The tag user identifier'),
+    'created_at': fields.String(readonly=True, description='The tag create datetime'),
+    'updated_at': fields.String(readonly=True, description='The tag update datetime'),
 })
 
 
 @tag_ns.route('')
 class TagList(Resource):
-    @tag_ns.doc('create-tag')
+    @tag_ns.doc('tag.create')
     @tag_ns.expect(tag)
     @tag_ns.marshal_with(tag, code=201)
     def post(self):
         """Create a tag"""
-        print(tag_ns.payload)
-        return tag_ns.payload
+        tag_mdl = TagDTO.to_tag(tag_ns.payload)
+        TagRepository.create_or_fail(tag_mdl)
+        return tag_mdl
+
