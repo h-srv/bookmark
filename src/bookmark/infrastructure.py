684d0e888a0ab18522425056b5c01be0d53b333e
HSN <hasanlock@gmail.com>
2022-04-17 01:49:31 +0600

    feat: init cerberus.Validator

--- src/bookmark/infrastructure.py
+++ src/bookmark/infrastructure.py
@@ -1,3 +1,4 @@
+from cerberus import Validator
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -5,3 +6,4 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 cors = CORS()
 migrate = Migrate()
+validator = Validator()
