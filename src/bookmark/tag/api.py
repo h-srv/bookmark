f5add059df14372624e5ac94f8a06d2d4eea327c
HSN <hasanlock@gmail.com>
2022-04-20 16:22:18 +0600

    refactor: style and docs

--- src/bookmark/tag/api.py
+++ src/bookmark/tag/api.py
@@ -8,11 +8,9 @@ blueprint_tag = Blueprint('blueprint_tag', __name__)
 
 @blueprint_tag.get('')
 def tag_list():
+    """return tag list for user_rel"""
     list_schema = {
-        'user_rel': {
-            'type': 'string',
-            'required': True
-        }
+        'user_rel': {'type': 'string', 'required': True}
     }
 
     if not validator.validate(request.args, list_schema):
