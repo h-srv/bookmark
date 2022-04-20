95b6a924f8bf039acaa3eb4b060a365156aaa959
HSN <hasanlock@gmail.com>
2022-04-20 16:58:56 +0600

    feat: better error handler

--- src/bookmark/infrastructure.py
+++ src/bookmark/infrastructure.py
@@ -10,9 +10,22 @@ validator = Validator()
 
 
 def exception_handler(ex):
-    (message, code) = ex.args
-    msg = dict(message=message)
+    from sqlalchemy.exc import IntegrityError
 
+    msg, code = {}, 500
+
+    if type(ex) is ValueError:  # if validation error
+        (message, c) = ex.args
+        msg, code = dict(ok=0, message=message), c
+
+    elif type(ex) is IntegrityError:  # if db error
+        if 'Duplicate entry' in str(ex):
+            msg, code = dict(ok=0, message='Duplicate entry found'), 400
+
+    if len(msg) == 0:  # if empty msg then throw generic msg
+        msg = dict(ok=0, message='Unknown system exception')
+
+    # add traceback if debug
     from flask import current_app
     if current_app.testing:
         import traceback
